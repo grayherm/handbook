c5b9722de3327b6b4878813570d71490a9cbfc1a
Zeue <5887963@gmail.com>
2018-10-15 20:01:32 +0000

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,48 +4,60 @@ module.exports = {
     base: '/',
     dest: 'public',
     head: [
-        ['link', { rel: 'icon', href: '/uagLogo.png' }]
+        ['link', {
+            rel: 'icon',
+            href: '/uagLogo.png'
+        }]
     ],
     serviceWorker: true,
     themeConfig: {
-		sidebar: {
-		  '/ch0/': [
-			'',
-			[ "getting-started", "Section 1: Getting Started" ],
-			'reserves-system'
-		  ]
-		},
-		sidebarDepth: 4,
-		displayAllHeaders: false,
-		activeHeaderLinks: false,
-		lastUpdated: true,
-		nav: [
-			{
-				text: 'Chapters',
-			items: [
-				{ text: 'Chapter 0: An Introduction', link: '/ch0/' },
-				{ text: 'Chapter 1: The Basics', link: '/ch1/' },
-				{ text: 'Chapter 2: Advanced Techniques', link: '/ch2/' },
-				{ text: 'Chapter 3: Leadership', link: '/ch3/' }
-			]
-			}
-		],
-		serviceWorker: { 
-			updatePopup: {
-				message: 'This page just got updated!',
-			buttonText: 'Refresh?'
-			}
-		},
-		repo: 'https://gitlab.com/uag/handbook',
-		repoLabel: 'Contribute!',
-		docsDir: 'docs',
-		editLinks: true,
-		editLinkText: 'Help us improve this page!'
+        sidebar: {
+            '/ch0/': [
+                '',
+                ["getting-started", "Section 1: Getting Started"],
+                'reserves-system'
+            ]
+        },
+        sidebarDepth: 4,
+        displayAllHeaders: false,
+        activeHeaderLinks: false,
+        lastUpdated: true,
+        nav: [{
+            text: 'Chapters',
+            items: [{
+                    text: 'Chapter 0: An Introduction',
+                    link: '/ch0/'
+                },
+                {
+                    text: 'Chapter 1: The Basics',
+                    link: '/ch1/'
+                },
+                {
+                    text: 'Chapter 2: Advanced Techniques',
+                    link: '/ch2/'
+                },
+                {
+                    text: 'Chapter 3: Leadership',
+                    link: '/ch3/'
+                }
+            ]
+        }],
+        serviceWorker: {
+            updatePopup: {
+                message: 'This page just got updated!',
+                buttonText: 'Refresh?'
+            }
+        },
+        repo: 'https://gitlab.com/uag/handbook',
+        repoLabel: 'Contribute!',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Help us improve this page!'
     },
-	markdown: {
-		lineNumbers: true
-	},
-	plugins: [
-		'flowchart'
-	]
+    markdown: {
+        lineNumbers: true
+    },
+    plugins: [
+        'flowchart'
+    ]
 }
