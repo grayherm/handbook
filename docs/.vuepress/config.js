196c1f287a6cacbd6de6be30c32c8052c604fbd3
Alex <alex@peccy.net>
2018-10-15 15:19:34 +0000

    add logo to header icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,8 @@ module.exports = {
     title: 'UAG Handbook',
     description: 'A handy tome for the scholarly contractor',
     base: '/',
-    dest: 'public'
+    dest: 'public',
+    head: [
+        ['link', { rel: 'icon', href: '/uagLogo.png' }]
+    ]
 }
